a5a059b5460a8ea9af59d28482d6900a4131328b
FuZi.D <foutz_is@hotmail.com>
2025-09-28 17:53:43 +0300

    added animation on container counter

--- Assets/Scripts/ContainerCounter.cs
+++ Assets/Scripts/ContainerCounter.cs
@@ -1,7 +1,9 @@
+using System;
 using UnityEngine;
 
 public class ContainerCounter : BaseCounter
 {
+    public event EventHandler OnPlayerGrabbedObject;
 
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
     public override void Interact(Player player)
@@ -10,7 +12,7 @@ public class ContainerCounter : BaseCounter
         {
             Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
             kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
-
+            OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
         }
 
     }
