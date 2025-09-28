2f09b75360360e5bead44efb02168ed2b942c60e
FuZi.D <foutz_is@hotmail.com>
2025-09-28 13:27:14 +0300

    Added Scriptable Objects for Ingredients

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
-   public void Interact()
-   {
+   [SerializeField] private KitchenObjectSO kitchenObjectSO;
+   [SerializeField] private Transform counterTopPoint;
+    public void Interact()
+    {
     Debug.Log("Interacted with " + transform.name);
-   }
+    Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+    kitchenObjectTransform.localPosition = Vector3.zero;
+
+    Debug.Log("Spawned " + kitchenObjectSO.name + " on counter.");
+    }
 }
